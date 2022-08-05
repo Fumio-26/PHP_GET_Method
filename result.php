200545958a04910ae48ee5022abf5199e8804168
Fumio_ <89429493+Fumio-26@users.noreply.github.com>
2022-08-05 18:06:41 +0700

    Add files via upload

---
+++ result.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Running Text Online dengan metode GET.">
+    <meta name="author" content="Axel">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET Method PHP - Fumioo</title>
+  </head>
+</html>
+
+<?php
+          echo "<marquee>", $_GET['text'], "</marquee>"; //Hasil-1
+          echo "Hasil: <strong>", $_GET['text']; //Hasil-2
+?>
